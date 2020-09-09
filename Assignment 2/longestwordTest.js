0a4ded525f41713f45b9d22b20e51afea3af19af
tahirmama351 <tahirmama@gmail.com>
2020-09-08 23:11:36 -0500

    uptodate

--- Assignment 2/longestwordTest.js
+++ Assignment 2/longestwordTest.js
@@ -6,7 +6,7 @@
 
 
 discribe("longestWord", function(){
-     it("the largest of tahir,mama, and maymuna is maymuna", function(){
+     it("the largest of tahir, mama, and maymuna is maymuna", function(){
      assert.equal(longestWord("tahir","mama","maymuna"), "maymuna")
      });
 });
