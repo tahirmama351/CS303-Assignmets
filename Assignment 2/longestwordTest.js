a94c525c1e87ef0cf4bc217df392ce423b51e432
tahirmama351 <tahirmama@gmail.com>
2020-09-09 14:01:22 -0500

    uptodate

--- Assignment 2/longestwordTest.js
+++ Assignment 2/longestwordTest.js
@@ -1,12 +1,10 @@
 'use strict'
-/**
- * @ parmeter {ara}
- */
 
-
+/* global assert longestWord*/
+/* longestWord() that takes an array of string and returns the length of longest word*/
 
 discribe("longestWord", function(){
      it("the largest of tahir, mama, and maymuna is maymuna", function(){
-     assert.equal(longestWord("tahir","mama","maymuna"), "maymuna")
+     assert.equal(longestWord("tahir","mama","maymuna"), 7)
      });
 });
